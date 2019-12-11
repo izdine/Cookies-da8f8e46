576e6fa3d0de90ab7674446b807ea4034e7c5cf4
Iz-Dine Amadou <2088346@talnet.nl>
2019-12-11 15:50:50 +0100

    Uitwerking van de exercise

---
+++ cart.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bit Academy</title>
+    </head>
+
+    <body>
+        <h1>Skateboard <small>(#1)</small></h1>
+        <h1>Basketbal <small>(#2)</small></h1>
+        <h1>Skeelers <small>(#3)</small></h1>
+
+        <!-- maak hier de opdracht -->
+        <input type="text" id="keuze "placeholder="Keuze" name=>
+        <input type="submit" name="button" value="Submit"></input>
+      <?php
+  session_start();
+
+       ?>
+    </body>
+</html>
