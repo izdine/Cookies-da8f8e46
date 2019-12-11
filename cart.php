2527e022ed61096484009b7dba9ea014d5b07060
Iz-Dine Amadou <2088346@talnet.nl>
2019-12-11 16:01:12 +0100

    Uitwerking van de exercise

--- cart.php
+++ cart.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,16 +7,32 @@
     </head>
 
     <body>
+      <form method="post">
+
         <h1>Skateboard <small>(#1)</small></h1>
         <h1>Basketbal <small>(#2)</small></h1>
         <h1>Skeelers <small>(#3)</small></h1>
 
-        <!-- maak hier de opdracht -->
-        <input type="text" id="keuze "placeholder="Keuze" name=>
+        <input type="text" id="keuze "placeholder="Keuze" name=Keuze>
         <input type="submit" name="button" value="Submit"></input>
-      <?php
-  session_start();
+    </form>
 
+      <?php
+      session_start();
+  $cookie1 = "Skateboard";
+  $cookie2 = "Basketbal";
+  $cookie3 = "Skeelers";
+  setcookie("COOKIE" , $_POST["Keuze"]);
+  if (isset($_POST["Keuze"])) {
+    setcookie("COOKIE", $_POST["Keuze"], time()+3600);
+    echo "<h1> Gekozen item: " . $_POST["Keuze"] . "</h1>";
+
+}
+
+
+elseif (isset($_COOKIE["Keuze"])) {
+  echo "<h1> Gekozen item: " . $_COOKIE["COOKIE"] . "</h1>";
+}
        ?>
     </body>
 </html>
